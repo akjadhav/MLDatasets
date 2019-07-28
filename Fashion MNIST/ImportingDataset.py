7e46bb3735c9abc05977fcb3e94f2aa1aad0212c
Ameya Jadhav <jadhavameyak@gmail.com>
2019-07-28 16:32:10 -0400

    Update ImportingDataset.py

--- Fashion MNIST/ImportingDataset.py
+++ Fashion MNIST/ImportingDataset.py
@@ -2,7 +2,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 data = input_data.read_data_sets('data/fashion', source_url='http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/')
 
-trainX = data.train.images
-trainY = data.train.labels
-testX = data.test.images
-testY = data.test.labels
+X_train = data.train.images
+y_train = data.train.labels
+X_test = data.test.images
+y_test = data.test.labels
